4f84f7554b99afc8350b498bbed423b38b10f631
Tulir293 <tulir293@maunium.net>
2015-04-20 21:04:30 +0300

    Added the Heat Seeking Missile bow enchantment

---
+++ src/net/maunium/bukkit/Maunchants/Enchants/HeatSeekingMissile.java
@@ -0,0 +1,97 @@
+package net.maunium.bukkit.Maunchants.Enchants;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.util.Vector;
+
+import net.maunium.bukkit.Maunchants.MauEnchant;
+import net.maunium.bukkit.Maunchants.Maunchants;
+
+public class HeatSeekingMissile implements MauEnchant, Listener {
+	public static final String MISSILE_META = "MaunchantsHeatSeekingMissile";
+	private Maunchants plugin;
+	private boolean loaded;
+	
+	@Override
+	public void load(Maunchants plugin) {
+		this.plugin = plugin;
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+		loaded = true;
+	}
+	
+	@Override
+	public void unload() {
+		HandlerList.unregisterAll(this);
+		plugin = null;
+		loaded = false;
+	}
+	
+	@Override
+	public boolean isLoaded() {
+		return loaded;
+	}
+	
+	@Override
+	public String getDisplayName() {
+		return plugin.translate("enchant.heatmissile.itemname");
+	}
+	
+	@EventHandler
+	public void onProjectileShoot(ProjectileLaunchEvent evt) {
+		if (evt.getEntity().getShooter() instanceof Player && evt.getEntity() instanceof Arrow) {
+			Player p = (Player) evt.getEntity().getShooter();
+			if (p.getItemInHand().getType().equals(Material.BOW) && p.getItemInHand().getItemMeta().hasDisplayName()
+					&& p.getItemInHand().getItemMeta().getDisplayName().equals(getName())) {
+				new SeekRunnable((Arrow) evt.getEntity(), 10).start();
+			}
+		}
+	}
+	
+	public class SeekRunnable implements Runnable {
+		private Arrow e;
+		private int recalculations, taskId;
+		
+		public SeekRunnable(Arrow e, int recalculations) {
+			this.e = e;
+			this.recalculations = recalculations;
+		}
+		
+		public void start() {
+			taskId = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, this, 20, 20);
+		}
+		
+		@Override
+		public void run() {
+			if (e == null || e.getVelocity().lengthSquared() == 0 || e.isDead() || recalculations < 1) {
+				plugin.getServer().getScheduler().cancelTask(taskId);
+				return;
+			}
+			Player nearest = null;
+			double nearestDist = Double.MAX_VALUE;
+			for (Entity v : e.getNearbyEntities(10, 5, 10)) {
+				if (v instanceof Player) {
+					double dist = v.getLocation().distanceSquared(e.getLocation());
+					if (dist < nearestDist) {
+						nearest = (Player) v;
+						nearestDist = dist;
+					}
+				}
+			}
+			
+			if (nearest != null) {
+				Vector from = new Vector(e.getLocation().getX(), e.getLocation().getY(), e.getLocation().getZ());
+				Vector to = new Vector(nearest.getLocation().getX(), nearest.getLocation().getY(), nearest.getLocation().getZ());
+				
+				e.setVelocity(to.subtract(from));
+			}
+			
+			recalculations--;
+		}
+	}
+}
