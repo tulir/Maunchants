9954d4dae91c5d1e24ad330be917fc14f2d120df
Tulir293 <tulir293@maunium.net>
2015-04-20 21:28:11 +0300

    Added maunchantments hashmap and all things needed

--- src/net/maunium/bukkit/Maunchants/Maunchants.java
+++ src/net/maunium/bukkit/Maunchants/Maunchants.java
@@ -1,25 +1,35 @@
 package net.maunium.bukkit.Maunchants;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.maunium.bukkit.Maunchants.Enchants.HeatSeekingMissile;
 import net.maunium.bukkit.Maussentials.Utils.I18n;
 
 public class Maunchants extends JavaPlugin {
-	private final String stag = ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "Maunchants" + ChatColor.DARK_GREEN + "] " + ChatColor.GRAY,
+	public final String stag = ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "Maunchants" + ChatColor.DARK_GREEN + "] " + ChatColor.GRAY,
 			errtag = ChatColor.DARK_RED + "[" + ChatColor.RED + "Maunchants" + ChatColor.DARK_RED + "] " + ChatColor.RED;
+	private Map<String, MauEnchant> maunchantments;
 	private I18n i18n;
 	
 	@Override
 	public void onEnable() {
 		long st = System.currentTimeMillis();
-		// TODO If plugin uses config, uncomment the following line.
-		// saveDefaultConfig();
 		
-		// TODO Enable code
+		maunchantments = new HashMap<String, MauEnchant>();
+		
+		add(new HeatSeekingMissile(this));
 		
 		int et = (int) (System.currentTimeMillis() - st);
 		getLogger().info("Maunchants v" + getDescription().getVersion() + " by Tulir293 enabled in " + et + "ms.");
+	}
+	
+	public void add(MauEnchant ench) {
+		maunchantments.put(ench.getName(), ench);
 	}
 	
 	@Override
@@ -30,6 +40,10 @@ public class Maunchants extends JavaPlugin {
 		
 		int et = (int) (System.currentTimeMillis() - st);
 		getLogger().info("Maunchants v" + getDescription().getVersion() + " by Tulir293 disabled in " + et + "ms.");
+	}
+	
+	public MauEnchant getEnch(String name) {
+		return maunchantments.get(name.toLowerCase(Locale.ENGLISH));
 	}
 	
 	public String translate(String node, Object... arguments) {
