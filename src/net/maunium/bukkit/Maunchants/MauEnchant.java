2d70bc9baadadb6741f36400a3a52da07c686bc7
Tulir293 <tulir293@maunium.net>
2015-04-20 21:27:51 +0300

    Finished MauEnchant base

--- src/net/maunium/bukkit/Maunchants/MauEnchant.java
+++ src/net/maunium/bukkit/Maunchants/MauEnchant.java
@@ -2,10 +2,16 @@ package net.maunium.bukkit.Maunchants;
 
 import java.util.Locale;
 
+import org.bukkit.entity.Player;
+
 public interface MauEnchant {
+	public void apply(Player p);
+	
+	public void remove(Player p);
+	
 	public default String getName() {
 		return getClass().getSimpleName().toLowerCase(Locale.ENGLISH);
 	}
 	
-	public String getDisplayName();
+	public String getLoreLine();
 }
